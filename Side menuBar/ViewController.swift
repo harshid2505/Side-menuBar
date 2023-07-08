3c393a117da125d67e2376efe7ee3495e7fb7f33
Harshid Vaddoriya <harshidpatel388@gmail.com>
2023-07-08 11:59:53 +0530

    Initial Commit

---
+++ Side menuBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Side menuBar
+//
+//  Created by HARSHID PATEL on 08/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
